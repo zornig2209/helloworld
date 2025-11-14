7716b06776112da16a6c4b753dd38d5616e6f543
marek <marek@DESKTOP-J5SAC82>
2025-11-14 14:18:20 +0100

    Projektdateien hinzufügen.

---
+++ helloworld.cpp
@@ -0,0 +1,12 @@
+﻿// helloworld.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "helloworld.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
